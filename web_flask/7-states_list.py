75c5f4f8728d5d438d3af8fe726e3a8e5c5b8b56
Semane S. Fentaye <114406855+semane12@users.noreply.github.com>
2023-05-20 08:55:37 +0300

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def states_list():
     States are sorted by name.
     """
     states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return render_templates("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
